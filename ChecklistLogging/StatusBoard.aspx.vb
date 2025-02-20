3553e3dd1b877280492f93fb4fe0e2b2355b35d9
Szymon Tyburek <STyburek@purewaferinc.az>
2025-02-20 13:01:42 -0700

    StatusBoard.aspx: organizing checklists by alphabetical order

--- ChecklistLogging/StatusBoard.aspx.vb
+++ ChecklistLogging/StatusBoard.aspx.vb
@@ -80,7 +80,7 @@ Partial Class MR_OpenTicketStatusBoard
         End If
 
         'build button controls for checklists that have a department, interval, assignee, & at least 1 input
-        AreaDS = SatiCode.GetMyDataSet("SELECT A.[Key] FROM [ALTS].[dbo].[T_LogArea] A INNER JOIN [ALTS].[dbo].[T_LogDepartment] D ON A.DepartmentKey=D.[Key] INNER JOIN [ALTS].[dbo].[T_LogAreaInterval] I ON A.IntervalKey=I.[Key] WHERE A.Active=1 AND" & If(Session("DepartmentFromQueryString") <> "All", " D.Department='" & Session("DepartmentFromQueryString") & "' AND", String.Empty) & " (SELECT COUNT([Key]) FROM [ALTS].[dbo].[T_LogLabel] L WHERE L.AreaKey=A.[Key]) > 0 AND A.Assignee IS NOT NULL ORDER BY I.DisplayOrder")
+        AreaDS = SatiCode.GetMyDataSet("SELECT A.[Key] FROM [ALTS].[dbo].[T_LogArea] A INNER JOIN [ALTS].[dbo].[T_LogDepartment] D ON A.DepartmentKey=D.[Key] INNER JOIN [ALTS].[dbo].[T_LogAreaInterval] I ON A.IntervalKey=I.[Key] WHERE A.Active=1 AND" & If(Session("DepartmentFromQueryString") <> "All", " D.Department='" & Session("DepartmentFromQueryString") & "' AND", String.Empty) & " (SELECT COUNT([Key]) FROM [ALTS].[dbo].[T_LogLabel] L WHERE L.AreaKey=A.[Key]) > 0 AND A.Assignee IS NOT NULL ORDER BY I.DisplayOrder, A.Area")
         AreaRC = AreaDS.Tables(0).Rows.Count
 
         For I = 0 To AreaRC - 1
