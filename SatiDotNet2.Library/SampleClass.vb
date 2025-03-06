d9b6d3ab058543784bf15c74d6c5c9b58d14aca7
Szymon Tyburek <STyburek@purewaferinc.az>
2025-03-06 10:50:22 -0700

    moved .git folder and .gitignore file to parent directory of SatiDotNet2, SatiDotNet2.Library, & SatiDotNet2Tests

---
+++ SatiDotNet2.Library/SampleClass.vb
@@ -0,0 +1,10 @@
+﻿Public Class SampleClass
+    Public Function AddNumbers(a As Integer, b As Integer) As Integer
+        Return a + b
+    End Function
+
+    Public Function ReturnTrue() As Boolean
+        Return True
+    End Function
+
+End Class
