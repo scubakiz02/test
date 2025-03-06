813a8fb2bdb3206ca597ccbef8063a124512442a
Szymon Tyburek <STyburek@purewaferinc.az>
2025-03-06 11:36:38 -0700

    moved .git folder and .gitignore file back to parent directory of solution, and finished unit tests & LogAspx.ValidateByBackColor

---
+++ SatiDotNet2.Library/ChecklistLogging/LogAspxLibrary.vb
@@ -0,0 +1,33 @@
+﻿Public Class LogAspx
+    'return value of: true is valid; false is invalid; null is out of range
+    Public Function ValidateByBackColor(NumOfNotes As Integer, BackColor As String) As Boolean?
+        'If NumOfNotes = 0 Then
+        '    If Value = "" OrElse Not All_InputsAreValid Then
+        '        MessageUserLabel.Text = "Error: Incomplete or invalid logs. Add a comment to proceed."
+        '        Exit Sub
+        '    End If
+        'ElseIf Not All_InputsAreValid Then
+        '    'display verify interface
+        '    DoneButton.Enabled = False
+        '    MarkAsDoneCheckBox.Visible = True
+        '    Return
+        'End If
+
+        Dim Res As Boolean = Nothing
+
+        If NumOfNotes > 0 OrElse BackColor = "#F5F5F5" Then
+            Res = True
+        ElseIf BackColor = "red" Then
+            Res = False
+        ElseIf BackColor = "#E6E600" Then 'yellow
+            Return Nothing 'cannot assign Nothing to variable Res for some reason
+        End If
+
+        Return Res
+    End Function
+
+    Public Function ReturnTrue() As Boolean
+        Return True
+    End Function
+
+End Class
---
+++ SatiDotNet2Tests/ChecklistLogging/LogAspxTests.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Text
+Imports Xunit
+Imports SatiDotNet2.Library
+
+Public Class LogAspxTests
+    Dim LogAspx = New LogAspx()
+
+    <Fact>
+    Public Sub ReturnTrueTest1()
+        Assert.True(LogAspx.ReturnTrue())
+    End Sub
+
+    <Fact>
+    Public Sub ValidateBackColorTest1()
+        'if num of notes > 0, regardless of back color, return true
+        Assert.True(LogAspx.ValidateByBackColor(1, "red"))
+    End Sub
+
+    <Fact>
+    Public Sub ValidateBackColorTest2()
+        'if num of notes is 0, and backcolor is red, return false
+        Assert.False(LogAspx.ValidateByBackColor(0, "red"))
+    End Sub
+
+    <Fact>
+    Public Sub ValidateBackColorTest3()
+        'if num of notes is 0, and backcolor is yellow, return nothing
+        Dim Res As Boolean? = LogAspx.ValidateByBackColor(0, "#E6E600")
+        Assert.True(If(Res Is Nothing, True, False))
+    End Sub
+
+    <Fact>
+    Public Sub ValidateBackColorTest4()
+        'if backcolor is gray, return true
+        Assert.True(LogAspx.ValidateByBackColor(0, "#F5F5F5"))
+    End Sub
+
+
+End Class
