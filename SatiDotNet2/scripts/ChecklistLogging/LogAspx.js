96a5e1780bf2d4920af19cb6532214ceb9dae1b5
Szymon Tyburek <STyburek@purewaferinc.az>
2025-03-14 11:57:38 -0700

    created a new folder for ChecklistLogging js in scripts folder. Running a successful jest test

---
+++ SatiDotNet2/scripts/ChecklistLogging/LogAspx.js
@@ -0,0 +1,4 @@
+﻿function returnTrue() {
+    return true;
+}
+module.exports = { returnTrue };
---
+++ SatiDotNet2/scripts/ChecklistLogging/LogAspx.test.js
@@ -0,0 +1,5 @@
+﻿const LogAspx = require("./LogAspx"); // Import the function
+
+test("call function and ensure it returns true", () => {
+    expect(LogAspx.returnTrue()).toBeTruthy();
+});
