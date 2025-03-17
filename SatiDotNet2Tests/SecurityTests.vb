990ac507c7be60766dcc881355d54b73d6b68336
Szymon Tyburek <STyburek@purewaferinc.az>
2025-03-17 11:19:54 -0700

    added extra tests to check sql injection based on boolean '1=1'. commented out these tests, b/c I'm holding off on using NoSqlInjection function and instead ONLY using parameterized queries to prevent sql injection

--- SatiDotNet2Tests/SecurityTests.vb
+++ SatiDotNet2Tests/SecurityTests.vb
@@ -15,6 +15,7 @@ Public Class SecurityTests
         Assert.True(Security.NoSqlInjection(""))
     End Sub
 
+    'testing against destructive sql commands
     <Fact>
     Public Sub NoSqlInjectionTest2()
         Assert.False(Security.NoSqlInjection("DROP TABLE DummyTable"))
@@ -29,5 +30,19 @@ Public Class SecurityTests
     Public Sub NoSqlInjectionTest4()
         Assert.False(Security.NoSqlInjection("select * from DummyTable"))
     End Sub
+    'testing against destructive sql commands
 
+    'testing against SQL Injection based on int=int
+    '<Fact>
+    'Public Sub NoSqlInjectionTest5()
+    '    'SELECT UserId, Name, Password FROM Users WHERE UserId = 105 or 1=1;
+    '    Assert.False(Security.NoSqlInjection("105 OR 1=1"))
+    'End Sub
+
+    '<Fact>
+    'Public Sub NoSqlInjectionTest6()
+    '    'SELECT UserId, Name, Password FROM Users WHERE UserId = 105 or 1=1;
+    '    Assert.False(Security.NoSqlInjection("105 or 4279=4279"))
+    'End Sub
+    'testing against SQL Injection based on int=int
 End Class
