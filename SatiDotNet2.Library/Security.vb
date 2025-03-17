118ddaff88bbcfca8498e431eecba37d0bff4c85
Szymon Tyburek <STyburek@purewaferinc.az>
2025-03-17 10:35:53 -0700

    view previous commit

---
+++ SatiDotNet2.Library/Security.vb
@@ -0,0 +1,10 @@
+﻿Public Class Security
+    Public Function NoSqlInjection(str As String) As Boolean
+        Return True
+    End Function
+
+    Public Function ReturnTrue() As Boolean
+        Return True
+    End Function
+
+End Class
---
+++ SatiDotNet2Tests/SecurityTests.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Text
+Imports Xunit
+Imports SatiDotNet2.Library
+
+Public Class SecurityTests
+    Dim Security = New Security()
+
+    <Fact>
+    Public Sub ReturnTrueTest1()
+        Assert.True(Security.ReturnTrue())
+    End Sub
+
+    <Fact>
+    Public Sub NoSqlInjectionTest1()
+        Assert.True(Security.NoSqlInjection(""))
+    End Sub
+
+End Class
