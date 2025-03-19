93119e48a83e7625ab0b86eeb7f4a02ab4a06324
Szymon Tyburek <STyburek@purewaferinc.az>
2025-03-19 08:24:41 -0700

    merged ChecklistBuilderAspxLibrary ModifyLabelOrder & ModifyCommentOrder to get ModifyOrder. CommentOrderTests & LabelOrderTests unit test classes call ModifyOrder, and are passing

--- SatiDotNet2Tests/ChecklistLogging/ChecklistBuilderAspxTests.vb
+++ SatiDotNet2Tests/ChecklistLogging/ChecklistBuilderAspxTests.vb
@@ -10,25 +10,25 @@ Public Class LabelOrderTests
     <Fact>
     Public Sub LabelOrder1()
         'moving label 1 up on Nitrogen Daily checklist
-        Assert.Equal("", ChecklistBuilderAspx.ModifyLabelOrder("388", "up"))
+        Assert.Equal("", ChecklistBuilderAspx.ModifyOrder("388", "up", "Label"))
     End Sub
 
     <Fact>
     Public Sub LabelOrder2()
         'moving label 2 up on Nitrogen Daily checklist
-        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=1 WHERE [Key]=389; UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=2 WHERE [Key]=388", ChecklistBuilderAspx.ModifyLabelOrder("389", "up"))
+        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=1 WHERE [Key]=389; UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=2 WHERE [Key]=388", ChecklistBuilderAspx.ModifyOrder("389", "up", "Label"))
     End Sub
 
     <Fact>
     Public Sub LabelOrder3()
         'moving label 3 down on Nitrogen Daily checklist
-        Assert.Equal("", ChecklistBuilderAspx.ModifyLabelOrder("390", "down"))
+        Assert.Equal("", ChecklistBuilderAspx.ModifyOrder("390", "down", "Label"))
     End Sub
 
     <Fact>
     Public Sub LabelOrder4()
         'moving label 2 down on Nitrogen Daily checklist
-        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=3 WHERE [Key]=389; UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=2 WHERE [Key]=390", ChecklistBuilderAspx.ModifyLabelOrder("389", "down"))
+        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=3 WHERE [Key]=389; UPDATE [ALTS].[dbo].[T_LogLabel] SET LabelOrder=2 WHERE [Key]=390", ChecklistBuilderAspx.ModifyOrder("389", "down", "Label"))
     End Sub
     'USING NITROGEN DAILY AS SAMPLE CHECKLIST. IF THE LABEL ORDER HAS CHANGED, THESE TESTS WILL FAIL!!!!!!!!!
 End Class
@@ -41,13 +41,13 @@ Public Class CommentOrderTests
     <Fact>
     Public Sub CommentOrder1()
         'moving comment 1 up on Nitrogen Daily checklist
-        Assert.Equal("", ChecklistBuilderAspx.ModifyCommentOrder("53", "up"))
+        Assert.Equal("", ChecklistBuilderAspx.ModifyOrder("53", "up", "Comment"))
     End Sub
 
     <Fact>
     Public Sub CommentOrder2()
         'moving comment 2 up on Nitrogen Daily checklist
-        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=1 WHERE [Key]=54; UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=2 WHERE [Key]=53", ChecklistBuilderAspx.ModifyCommentOrder("54", "up"))
+        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=1 WHERE [Key]=54; UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=2 WHERE [Key]=53", ChecklistBuilderAspx.ModifyOrder("54", "up", "Comment"))
     End Sub
 
     <Fact>
@@ -59,7 +59,7 @@ Public Class CommentOrderTests
     <Fact>
     Public Sub CommentOrder4()
         'moving comment 2 down on Nitrogen Daily checklist
-        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=3 WHERE [Key]=54; UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=2 WHERE [Key]=55", ChecklistBuilderAspx.ModifyCommentOrder("54", "down"))
+        Assert.Equal("UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=3 WHERE [Key]=54; UPDATE [ALTS].[dbo].[T_LogCommentList] SET CommentOrder=2 WHERE [Key]=55", ChecklistBuilderAspx.ModifyOrder("54", "down", "Comment"))
     End Sub
     'USING R.O Daily AS SAMPLE CHECKLIST. IF THE COMMENT ORDER HAS CHANGED, THESE TESTS WILL FAIL!!!!!!!!!
 End Class
